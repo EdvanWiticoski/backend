5ac275061cbd762a7157f9d48c0940aebca990ab
edvan <edvan2108@gmail.com>
2019-04-09 23:03:43 +1400

    Estrutura back-end

---
+++ src/controllers/FileController.js
@@ -0,0 +1,27 @@
+const Box = require('../model/Box');
+const File = require('../model/File');
+
+class FileController {
+    async store(req, res) {
+        const box = await Box.findById(req.params.id);
+
+        const file = await File.create({
+            title: req.file.originalname,
+            path: req.file.key
+        });
+
+        box.files.push(file);
+
+        await box.save();
+
+        req.io.sockets.in(box._id).emit('file', file);
+
+        return res.json(file);
+
+        /*   console.log(req.file);
+           return res.send('OK')*/
+
+    }
+};
+
+module.exports = new FileController();
